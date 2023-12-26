20d2eb152e6ec351eb90d667c898ca109cee5163
Eduard von Briesen <e.v.briesen@googlemail.com>
2023-12-26 13:35:52 +0100

    refactor: ♻️ moved single view component into meme view

--- frontend/src/views/Memes.vue
+++ frontend/src/views/Memes.vue
@@ -1,9 +1,70 @@
 <script setup lang="ts">
-import SingleView from "@/components/SingleView.vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { getMeme } from "@/utils/api";
+import PlayfulText from "@/components/PlayfulText.vue";
+
+const meme = ref<{
+  id: string;
+  base64: string;
+  userId: string;
+  timestamp: string;
+  upvotes: number;
+  downvotes: number;
+} | null>(null);
+
+onMounted(async () => {
+  const route = useRoute();
+  const memeId = route.params.memeId as string;
+  meme.value = await getMeme(memeId);
+});
+
+function downloadMeme() {
+  // Create a link element and trigger a click to download the image
+  const link = document.createElement("a");
+  const memeImage = document.getElementById("memeImage") as HTMLImageElement;
+  link.href = memeImage.src;
+  link.download = "my_meme.png";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+async function shareMeme() {
+  try {
+    const currentUrl = window.location.href;
+
+    await navigator.clipboard.writeText(currentUrl);
+
+    console.log("URL copied to clipboard:", currentUrl);
+  } catch (error) {
+    console.error("Error copying URL to clipboard:", error);
+  }
+}
 </script>
 
 <template>
-  <div class="p-16">
-    <SingleView />
+  <div class="flex flex-col items-center p-16">
+    <div class="flex w-fit flex-col justify-center gap-4">
+      <div class="card bg-neutral h-fit w-fit">
+        <div class="card-body">
+          <img id="memeImage" alt="Meme Image" :src="meme?.base64" />
+        </div>
+      </div>
+      <div class="flex justify-center gap-4">
+        <button class="btn btn-primary w-48" @click="downloadMeme">
+          Download
+        </button>
+        <button class="btn btn-primary w-48" @click="shareMeme">Share</button>
+      </div>
+    </div>
+    <div class="flex flex-col items-center gap-5 py-4">
+      <PlayfulText word="Comments" />
+      <input
+        type="text"
+        placeholder="Add a comment"
+        class="input input-bordered w-full max-w-xs"
+      />
+    </div>
   </div>
 </template>
