7bfbfdfd3e94584cb557f606814a43395f232a20
Eduard von Briesen <e.v.briesen@googlemail.com>
2024-01-07 13:49:55 +0100

    feat: ✨ `create` endpoint allows the creation of multiple memes at once

--- server/src/routers/meme.ts
+++ server/src/routers/meme.ts
@@ -88,48 +88,58 @@ export const memeRouter = router({
     .input(
       z.object({
         template: z.string(),
-        title: z.string(),
-        texts: z
+        memes: z
           .object({
-            text: z.string(),
-            x: z.number().default(0),
-            y: z.number().default(0),
-            size: z.number().default(20),
-            color: z.string().default("#000000"),
+            title: z.string(),
+            texts: z
+              .object({
+                text: z.string(),
+                x: z.number().default(0),
+                y: z.number().default(0),
+                size: z.number().default(20),
+                color: z.string().default("#000000"),
+              })
+              .array(),
           })
           .array(),
       }),
     )
     .output(z.any())
     .mutation(async ({ ctx, input }) => {
-      const image = await loadImage(input.template);
-      const canvas = createCanvas(image.width, image.height);
-      const canvasCtx = canvas.getContext("2d");
-      canvasCtx.drawImage(image, 0, 0, image.width, image.height);
+      const memes = [];
 
-      for (const text of input.texts) {
-        canvasCtx.font = `${text.size}px sans-serif`;
-        canvasCtx.fillStyle = text.color;
-        canvasCtx.fillText(text.text, text.x, text.y);
+      for (const meme of input.memes) {
+        const image = await loadImage(input.template);
+        const canvas = createCanvas(image.width, image.height);
+        const canvasCtx = canvas.getContext("2d");
+        canvasCtx.drawImage(image, 0, 0, image.width, image.height);
+
+        for (const text of meme.texts) {
+          canvasCtx.font = `${text.size}px sans-serif`;
+          canvasCtx.fillStyle = text.color;
+          canvasCtx.fillText(text.text, text.x, text.y);
+        }
+
+        const memeLink = await ctx.prisma.meme
+          .create({
+            data: {
+              user: {
+                connect: {
+                  id: "655b6b0c07a007a18d5ca219", // test-user
+                },
+              },
+              title: meme.title,
+              base64: canvas.toDataURL(),
+            },
+          })
+          .then((meme) => ({
+            link: `http://localhost:5173/meme/${meme.id}`,
+          }));
+
+        memes.push(memeLink.link);
       }
 
-      const meme = await ctx.prisma.meme
-        .create({
-          data: {
-            user: {
-              connect: {
-                id: "655b6b0c07a007a18d5ca219", // test-user
-              },
-            },
-            title: input.title,
-            base64: canvas.toDataURL(),
-          },
-        })
-        .then((meme) => ({
-          link: `http://localhost:5173/meme/${meme.id}`,
-        }));
-
-      return meme;
+      return memes;
     }),
   vote: privatProcedure
     .input(
