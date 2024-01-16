38d694ac00bbef67382564ea1601318518689c30
Eduard von Briesen <e.v.briesen@googlemail.com>
2024-01-16 16:57:22 +0100

    feat: ✨ added meme overview with visibility option to drafts page

--- frontend/src/views/Drafts.vue
+++ frontend/src/views/Drafts.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { deleteDraft, getAllDrafts } from "@/utils/api";
+import PlayfulText from "@/components/PlayfulText.vue";
+import { deleteDraft, getAllDrafts, client } from "@/utils/api";
 import { TrashIcon, PencilIcon } from "@heroicons/vue/24/solid";
 import { Ref, onMounted, ref } from "vue";
 
@@ -11,9 +12,26 @@ const drafts: Ref<
   }[]
 > = ref([]);
 
+const memes: Ref<
+  {
+    id: string;
+    description: string;
+    base64: string;
+    userId: string;
+    title: string;
+    visibility?: "PUBLIC" | "PRIVATE" | "UNLISTED";
+    timestamp: string;
+    upvotes: number;
+    downvotes: number;
+  }[]
+> = ref([]);
+
 onMounted(() => {
   getAllDrafts().then((data) => {
     drafts.value = data;
+  });
+  client.user.getMemes.query().then((data) => {
+    memes.value = data;
   });
 });
 
@@ -31,7 +49,50 @@ function deleteDraftHandler(id: string) {
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex flex-col items-center justify-center">
+    <PlayfulText word="Memes" />
+    <table class="bg-default table max-w-screen-sm">
+      <thead>
+        <tr>
+          <th>Preview</th>
+          <th>Title</th>
+          <th>Created</th>
+          <th>Visibility</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="meme in memes" :key="meme.id" class="hover">
+          <td>
+            <img
+              :src="meme.base64"
+              class="h-16 w-16"
+              @click="() => $router.push(`/meme/${meme.id}`)"
+            />
+          </td>
+          <td>{{ meme.title }}</td>
+          <td>{{ new Date(meme.timestamp).toLocaleString() }}</td>
+          <td class="flex gap-4">
+            <select
+              class="select select-primary select-bordered w-full"
+              id="visibility"
+              v-model="meme.visibility"
+              @change="
+                (e) =>
+                  client.user.updateMemeVisibility.mutate({
+                    id: meme.id,
+                    visibility: e.target.value,
+                  })
+              "
+            >
+              <option value="PUBLIC">Public</option>
+              <option value="UNLISTED">Unlisted</option>
+              <option value="PRIVATE">Private</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <PlayfulText word="Drafts" />
     <table class="bg-default table max-w-screen-sm">
       <thead>
         <tr>
