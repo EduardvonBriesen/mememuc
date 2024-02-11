0d5e46f6afe63ff650461ae7acd31027481c0f73
Eduard von Briesen <e.v.briesen@googlemail.com>
2024-02-11 10:31:12 +0100

    feat: ✨ show previously created memes when offline

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -24,10 +24,19 @@ const memes = ref<
 const page = ref(1);
 const endOfPage = ref(false);
 
+const isOffline = ref(false);
+
 onMounted(async () => {
-  getMemes({ sort: "new", image: true }).then((data) => {
-    memes.value = data;
-  });
+  getMemes({ sort: "new", image: true })
+    .then((data) => {
+      memes.value = data;
+    })
+    .catch((error) => {
+      console.error(error);
+      console.log("Loading memes from local storage");
+      memes.value = JSON.parse(localStorage.getItem("memes") || "[]");
+      isOffline.value = true;
+    });
 });
 
 function loadMore() {
@@ -46,6 +55,24 @@ function loadMore() {
 <template>
   <div class="flex flex-col items-center gap-4">
     <PlayfulText word="Memes" />
+    <div role="alert" class="alert alert-error max-w-md" v-if="isOffline">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 shrink-0 stroke-current"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>
+        You are currently offline. The memes are loaded from the local storage.
+      </span>
+    </div>
     <MemeCard v-for="meme in memes" :key="meme.id" :meme="meme" />
     <button @click="loadMore" class="btn btn-primary m-4" v-if="!endOfPage">
       <PlusCircleIcon class="h-5 w-5" />
